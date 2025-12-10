dd4c425a2bf71526fbedcd84cf7089513da9e6ab
shishir dwivedi <shishirdwivedi564@gmail.com>
2025-12-10 20:50:25 +0530

    done

--- COHORT0/server.js
+++ COHORT0/server.js
@@ -7,9 +7,5 @@ const server = http.createServer((req , res)=>{  // here create a server
 
 server.listen(3000, () => {        // here server listening a request
 console.log(" server start")
-<<<<<<< HEAD
 })  
 // COHORT 1 BHI ADD HO CHUKA HAI 
-=======
-})   
->>>>>>> 5535f14ed7799da6bb2bdbebb9f3f292c88f3799
