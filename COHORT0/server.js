603c4f75743a33f36714346c170d7f7f1b3913c2
shishir dwivedi <shishirdwivedi564@gmail.com>
2025-12-10 20:30:36 +0530

    p

---
+++ COHORT0/server.js
@@ -0,0 +1,11 @@
+
+const http = require('http') // use http to create server 
+
+const server = http.createServer((req , res)=>{  // here create a server 
+    res.end("server start ho chuka hai ")
+})
+
+server.listen(3000, () => {        // here server listening a request
+console.log(" server start")
+})  
+// COHORT 1 BHI ADD HO CHUKA HAI 
