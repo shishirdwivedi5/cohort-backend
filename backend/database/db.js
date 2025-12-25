372749ea8330c6db2bb058ee5d3578ad04eda130
SHISHIR DWIVEDI <shishirdwivedi564@gmail.com>
2025-12-25 23:46:30 +0530

    Delete backend directory

--- backend/database/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-function connectDB(){
-    mongoose.connect(process.env.DATABASE_URL)
-    .then(()=>{
-        console.log("Database connected successfully")
-    })
-} 
-
-module.exports = connectDB
--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app')
-
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000")
-})
-
-
-
-// yaha se hamara server listen kar raha hai
